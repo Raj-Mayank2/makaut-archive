c4f3e19a7b114758be8a3b1fc6ddeda2e5b25fa8
Raj-Mayank2 <mayank170403@gmail.com>
2025-09-03 17:45:53 +0530

    news

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,12 @@
 // src/services/api.js
 import axios from 'axios';
 
+
+const API_URL = 'https://notes-backend-ii2j.onrender.com' || 'http://localhost:5000';
+
 const api = axios.create({
   // IMPORTANT: Replace this with your actual backend URL when you build it
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: `${API_URL}/api`,
 });
 
 // This part is for sending the token with every request once the user is logged in
